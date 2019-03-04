15caf744a062a3e06207420917268f3b7d5810e2
Manu <ultragames007@gmail.com>
2019-03-04 20:27:20 -0300

    Painting Tool Test

--- sketch.js
+++ sketch.js
@@ -1,12 +1,23 @@
 function setup() {
-    createCanvas(603,603);
-}
+    createCanvas(400,400);
+    background(153, 153, 255);
+    }
+    var x=0;
+    var y=0;
+    var z=0;
 
 function draw() {
-    background(255,0,255);
-    strokeWeight(10); //hace todas las lineas gordas
-    line(0,201,603,201);
-    line(0,401,603,401);
-    line(201,0,201,603);
-    line(401,0,401,603);
+
+    noStroke();
+    //stroke(255,0,0); //borde
+    strokeWeight(3); //grosor
+    fill(200, mouseX-x, mouseY+z); //relleno va antes de dibujar
+    ellipse(mouseX,mouseY,50,50);
+}
+function mousePressed(){
+    background(153, 153, 255); //Limpia la pantalla
+    x=random(0,255);
+    y=random(0,255);
+    z=random(0,255);
+    print("El valor de x es",x,y,z);
 }
